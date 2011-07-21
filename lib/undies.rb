a192285a8be34eb62f220c9388e14909e2de05d4
Kelly <kelly@kelredd.com>
2011-07-20 22:06:00 -0500

    main undies require file

--- lib/undies.rb
+++ lib/undies.rb
@@ -1,3 +1,12 @@
+require 'undies/template'
+
 module Undies
+
+  # TODO: maybe implement this pattern??
+  # class << self
+  #   def new(&block)
+  #     Template.new(&block)
+  #   end
+  # end
 
 end
