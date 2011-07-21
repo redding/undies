b48a290eca731b9a91ed89aa056a63080a4962f1
Kelly <kelly@kelredd.com>
2011-07-21 14:07:05 -0500

    making the buffer class smarter so that both template and tag objects can use it

--- lib/undies/buffer.rb
+++ lib/undies/buffer.rb
@@ -1,10 +1,67 @@
 module Undies
   class Buffer < ::Array
 
+    # Add data and don't escape it
+    def __(data="")
+      self << data
+    end
+    # Add data and escape it
+    def _(data="")
+      self << escape_html(data)
+    end
+
+    TAG_METH_REGEX = /^_(.+)$/
+
+    def method_missing(meth, *args, &block)
+      if meth.to_s =~ TAG_METH_REGEX
+        tag($1, *args, &block)
+      else
+        super
+      end
+    end
+
+    def respond_to?(*args)
+      if args.first.to_s =~ TAG_METH_REGEX
+        true
+      else
+        super
+      end
+    end
+
+    def tag(name, attrs={}, &block)
+      self << Tag.new(name, attrs, &block)
+    end
+
     def to_s(pretty_print=false)
       # TODO: incorp pretty printing the HTML in the buffer
-      self.join('')
+      self.collect do |i|
+        begin
+          i.to_s(pretty_print)
+        rescue ArgumentError => err
+          i.to_s
+        end
+      end.join
     end
+
+    private
+
+    # Ripped from Rack v1.3.0 ======================================
+    # => ripped b/c I don't want a dependency on Rack for just this
+    ESCAPE_HTML = {
+      "&" => "&amp;",
+      "<" => "&lt;",
+      ">" => "&gt;",
+      "'" => "&#x27;",
+      '"' => "&quot;",
+      "/" => "&#x2F;"
+    }
+    ESCAPE_HTML_PATTERN = Regexp.union(*ESCAPE_HTML.keys)
+
+    # Escape ampersands, brackets and quotes to their HTML/XML entities.
+    def escape_html(string)
+      string.to_s.gsub(ESCAPE_HTML_PATTERN){|c| ESCAPE_HTML[c] }
+    end
+    # end Rip from Rack v1.3.0 =====================================
 
   end
 end
--- test/buffer_test.rb
+++ test/buffer_test.rb
@@ -1,18 +1,75 @@
 require "test/helper"
 require "undies/buffer"
+require "undies/tag"
 
 class Undies::Buffer
 
-  class BufferTest < Test::Unit::TestCase
+
+
+
+  class BasicTest < Test::Unit::TestCase
     include TestBelt
 
     context 'a buffer'
     subject { Undies::Buffer.new }
-    should have_instance_methods :to_s
+    should have_instance_methods :to_s, :_, :__, :tag
 
     should "be a kind of ::Array" do
       assert subject.kind_of?(::Array)
     end
+
   end
+
+
+
+  class DataTest < BasicTest
+    context "with data"
+    before do
+      @data = "stuff & <em>more stuff</em>"
+    end
+
+    should "add it un-escaped using the '__' method" do
+      subject.__ @data
+      assert_equal "stuff & <em>more stuff</em>", subject.to_s
+    end
+
+    should "add it escaped using the '_' method" do
+      subject._ @data
+      assert_equal "stuff &amp; &lt;em&gt;more stuff&lt;&#x2F;em&gt;", subject.to_s
+    end
+
+  end
+
+
+
+  class TagTest < BasicTest
+    context "when using the tag method"
+
+    should "add a new Tag object" do
+      subject.tag(:br)
+      assert_equal Undies::Tag.new(:br), subject.first
+    end
+
+    should "respond to any underscore prefix method" do
+      assert subject.respond_to?(:_div)
+    end
+
+    should "not respond to tag methods without an underscore prefix" do
+      assert !subject.respond_to?(:div)
+      assert_raises NoMethodError do
+        subject.div
+      end
+    end
+
+    should "interpret underscore prefix methods as a tag" do
+      assert_equal subject._div, subject.tag(:div)
+    end
+
+  end
+
+
+
+  # TODO: pretty printing
+
 
 end
