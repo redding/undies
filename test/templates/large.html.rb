99bb2366b0f6562977e394461d9fa4973d781fc1
Kelly <kelly@kelredd.com>
2011-10-26 16:50:09 -0500

    more dynamic large template to profile against

--- test/templates/large.html.rb
+++ test/templates/large.html.rb
@@ -1,153 +1,212 @@
 _html {
   _head {}
   _body {
+
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+    _ "Yo"
+
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+    __ "YoYo"
+
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+    _br
+
+
     _div { _ "Hi" }
     _div { _ "Hi" }
-    _div {
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-      _div { _ "Hi" }
-    }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+    _div { _ "Hi" }
+
   }
 }
